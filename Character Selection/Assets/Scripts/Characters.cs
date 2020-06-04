76d5ccb63b5368c28d19a9bd2cbd8b9e15c44112
Carcharocles-megalodon <55915879+Carcharocles-megalodon@users.noreply.github.com>
2020-06-04 08:42:38 +0100

    did this work? basic scene and script to make models dissapear

---
+++ Character Selection/Assets/Scripts/Characters.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Characters : MonoBehaviour
+{
+    [SerializeField] private GameObject[] charactersList;
+
+    private void Start()
+    {
+        charactersList = new GameObject[transform.childCount];
+
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            charactersList[i] = transform.GetChild(i).gameObject;
+        }
+        
+        foreach(GameObject go in charactersList)
+        {
+            go.SetActive(false);
+        }
+    }
+}
